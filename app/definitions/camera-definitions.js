db882bf52a3fbfbd9dc303334190f45294eeee25
Kostadin Spiridonov <kspiridonov@genscape.com>
2018-07-02 18:46:06 +0300

    Removed unused stuff

--- app/definitions/camera-definitions.js
+++
@@ -1,8 +0,0 @@
-export class CameraDefinitions {
-    static getBaseCamera() {
-        var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 5000);
-        camera.position.z = 1000;
-
-        return camera;
-    }
-}
--- app/definitions/scene-definitions.js
+++
@@ -1,9 +0,0 @@
-export class SceneDefinitions {
-    static getBaseScene() {
-        var scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xf0f0f0);
-        scene.add(new THREE.AmbientLight(0x505050));
-
-        return scene;
-    }
-}
